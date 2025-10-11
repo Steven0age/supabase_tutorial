bab7a22d8d2e3e614662648518507d287de2ea1d
Stephan Haak <info@stephan-haak.com>
2025-10-11 06:50:04 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>
+        Das werden meine ersten "Geh-Versuche" mit einem Backend und Login
+      </h2>
+    </>
+  );
+}
+
+export default App;
